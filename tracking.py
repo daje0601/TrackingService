5844a4d8259d809f2b1171af504972671308fc13
daje0601 <englishmt4118@gmail.com>
2020-12-08 23:25:25 +0900

    start(api key)
    project make a web site

---
+++ tracking.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://api.trackingmore.com"
+
+header = {
+	 "Content-Type" : "application / json" ,
+	 "Trackingmore-Api-Key" : "2e6f4905-ab10-4abd-8a5f-b1636127c3ae"
+}
+
