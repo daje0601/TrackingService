d4a00c67aa9c4eb53e0befcea2628206413035d3
daje0601 <englishmt4118@gmail.com>
2020-12-10 22:57:51 +0900

    add info

--- tracking.py
+++ tracking.py
@@ -8,7 +8,7 @@ import requests
 
 #우리가 원하는 사이트를 들어가서 사용자가 송장번호 입력할 수 있음 
 baseUrl = 'https://th.kerryexpress.com/th/track/?track='
-plusUrl = input("무엇을 검색할까요?:")
+plusUrl = # input("무엇을 검색할까요?:")
 #"sly2000312465"
 
 #입력된 송장번호로 검색 
@@ -23,6 +23,9 @@ request = requests.get(url)
 soup = BeautifulSoup(request.text, "html.parser")
 table = soup.find_all("div", {"class": "d-flex flex-column flex-sm-row flex-fill pl-3"})
 
-
-
+for info in table:
+    a = info.find("span", {"class":"text-1424"}).get_text()
+    b = info.find("span", {"class":"text-1418 light"}).get_text()
+    print(a, b)
+    
 driver.close()
